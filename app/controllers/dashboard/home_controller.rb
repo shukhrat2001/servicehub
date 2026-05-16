74d52e6d53e473272ac24b0cd9bb2db5d7e805ca
shukhrat2001@gmai.com <shukhrat2001@gmail.com>
2026-05-15 21:51:23 -0500

    Create dashboard controller

--- app/controllers/dashboard/home_controller.rb
+++ app/controllers/dashboard/home_controller.rb
@@ -1,4 +1,13 @@
-class Dashboard::HomeController < ApplicationController
-  def index
+module Dashboard
+  class HomeController < ApplicationController
+    before_action :authenticate_user!
+    layout 'dashboard'
+
+    def index
+      @customer_bookings = current_user.bookings.includes(:service).order(created_at: :desc)
+      @provider_bookings = Booking.joins(:service).where(services: { user_id: current_user.id }).includes(:service, :customer).order(created_at: :desc)
+      @received_messages = current_user.received_messages.includes(:sender, :service).order(created_at: :desc).limit(5)
+      @sent_messages = current_user.sent_messages.includes(:receiver, :service).order(created_at: :desc).limit(5)
+    end
   end
 end
