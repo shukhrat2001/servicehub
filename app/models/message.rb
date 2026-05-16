781f617c4afb4a95f09284447efa1d6df27b7953
shukhrat2001@gmai.com <shukhrat2001@gmail.com>
2026-05-15 21:28:50 -0500

    Update Message and Service.rb files

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,7 @@ class Message < ApplicationRecord
   belongs_to :service
   belongs_to :sender, class_name: "User"
   belongs_to :receiver, class_name: "User"
+
+  validates :content, presence: true
+  validates :sender_id, :receiver_id, :service_id, presence: true
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,7 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
   validates :base_price, presence: true
